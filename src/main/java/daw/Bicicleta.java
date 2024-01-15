105054c4aa35db5d9797cb1cb469c0d8b649d298
jrommir834e <jrommir834e@g.educaand.es>
2024-01-15 09:45:37 +0100

    Añade atributos en clase modelo

---
+++ src/main/java/daw/Bicicleta.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw;
+
+/**
+ *
+ * @author jesus
+ */
+public class Bicicleta {
+    
+    //Atributos encapsulados
+    
+    private int numRuedas;
+    private String modelo;
+    private String tipoCadena;
+}
---
+++ src/main/java/daw/Tarea5DJesus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author jesus
+ */
+public class Tarea5DJesus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
